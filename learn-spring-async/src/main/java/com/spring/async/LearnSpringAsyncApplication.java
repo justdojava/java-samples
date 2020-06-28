f64e397fe8b0fc4483b72f7fd8a044bc4ce397ff
xurj <xurj@lianlianpay.com>
2020-06-28 18:54:06 +0800

    learn-spring-async init

---
+++ learn-spring-async/src/main/java/com/spring/async/LearnSpringAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.spring.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringAsyncApplication.class, args);
+    }
+
+}
