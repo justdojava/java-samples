6b08de7f9d15c5853333759645ac94cebecfffd6
andy.xu <xu9529@gmail.com>
2020-08-03 07:42:38 +0800

    add spring-session-example

---
+++ spring-session-example/src/main/java/com/spring/session/SpringSessionExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSessionExampleApplication.class, args);
+    }
+
+}
---
+++ spring-session-example/src/test/java/com/spring/session/SpringSessionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSessionExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
