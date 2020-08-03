658b7d02faeef41215f18ed92f2057af2727fb2f
andy.xu <xu9529@gmail.com>
2020-08-03 08:41:57 +0800

    add spring session test

---
+++ spring-session-example/src/main/java/com/spring/session/HelloController.java
@@ -0,0 +1,36 @@
+package com.spring.session;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author andyXu xu9529@gmail.com
+ * @date 2020/8/3
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/test/cookie")
+    public String cookie(@RequestParam("browser") String browser, HttpServletRequest request, HttpSession session) {
+        //取出session中的browser
+        Object sessionBrowser = session.getAttribute("browser");
+        if (sessionBrowser == null) {
+            System.out.println("不存在session，设置browser=" + browser);
+            session.setAttribute("browser", browser);
+        } else {
+            System.out.println("存在session，browser=" + sessionBrowser.toString());
+        }
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                System.out.println(cookie.getName() + " : " + cookie.getValue());
+            }
+        }
+        return "index";
+    }
+}
---
+++ spring-session-example/src/main/java/com/spring/session/HttpSessionConfig.java
@@ -0,0 +1,11 @@
+package com.spring.session;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * @author andyXu xu9529@gmail.com
+ * @date 2020/8/3
+ */
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+}
