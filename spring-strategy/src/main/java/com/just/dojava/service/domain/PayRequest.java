2f61de781095a4f6b05a40a4f8f2897da240cff0
andy.xu <xu9529@gmail.com>
2020-09-22 22:12:40 +0800

    初始化策略类的工程

---
+++ spring-strategy/src/main/java/com/just/dojava/service/domain/PayRequest.java
@@ -0,0 +1,16 @@
+package com.just.dojava.service.domain;
+
+import lombok.Data;
+
+/**
+ * @author andyXu xu9529@gmail.com
+ * @date 2020/9/22
+ */
+@Data
+public class PayRequest {
+
+    /**
+     * 渠道编码
+     */
+    private String channelNo;
+}
